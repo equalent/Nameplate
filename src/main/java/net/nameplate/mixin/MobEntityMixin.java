94abf8c1f965f15ab133f21f84e7f4536e4dee43
Andrei Țurcan <6237485+equalent@users.noreply.github.com>
2022-10-15 14:58:07 +0400

    Update MobEntityMixin.java

--- src/main/java/net/nameplate/mixin/MobEntityMixin.java
+++ src/main/java/net/nameplate/mixin/MobEntityMixin.java
@@ -20,8 +20,7 @@ public class MobEntityMixin implements MobEntityAccess {
 
     @Inject(method = "<init>", at = @At("TAIL"))
     private void initMixin(EntityType<? extends MobEntity> entityType, World world, CallbackInfo info) {
-        if (Nameplate.CONFIG.excludedEntities.contains(((MobEntity) (Object) this).getType().toString().replace("entity.", "").replace(".", ":")))
-            this.showMobRpgLabel = false;
+        this.showMobRpgLabel = false;
     }
 
     @Inject(method = "writeCustomDataToNbt", at = @At("TAIL"))
